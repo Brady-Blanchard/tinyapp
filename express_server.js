decb2faec85d4cba278b86c419d0f04cf4ddb971
Brady-Blanchard <bradyjames97@hotmail.com>
2023-07-15 12:14:13 -0300

    Add and Implement cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require ("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// setup
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 // function to generate a random 6 character alphanumeric string to use for shortURL
 function generateRandomString() {
@@ -22,25 +27,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase,
+  };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
@@ -49,13 +53,25 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    username: req.cookies["username"],
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
+// generates a random shortURL and stores it inside the urldatabase as a key with a value of the longURL
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// submits the username as a cookie to login
 app.post("/login", (req, res) => {
-  res.cookie(req.body.username, "submitted");
-  res.redirect("/urls")
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
 });
 
 // deletes short urls from the urlDatabase
